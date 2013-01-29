438a2321c571125157e5a36e90a13faf0018494d
Jurriaan Bremer <jurriaanbremer@gmail.com>
2013-01-29 09:17:45 +0100

    omit superfluous if statements

--- triegen.py
+++ triegen.py
@@ -15,6 +15,15 @@ class Node:
         self.value = value
         self.right = right
 
+    def count(self):
+        """Returns the count of values, including its own."""
+        ret = 1
+        if self.left:
+            ret += self.left.count()
+        if self.right:
+            ret += self.right.count()
+        return ret
+
 
 def trie(l):
     """Function which translates a list into a balanced trie."""
@@ -45,14 +54,20 @@ def triegen_C_int(n, fmt):
     lines = []
 
     if n.left:
-        lines += ['if(x < %s) {' % n.value]
-        lines += [' ' + x for x in triegen_C_int(n.left, fmt)]
-        lines += ['}']
+        if n.left.count() == 1:
+            lines += triegen_C_int(n.left, fmt)
+        else:
+            lines += ['if(x < %s) {' % n.value]
+            lines += [' ' + x for x in triegen_C_int(n.left, fmt)]
+            lines += ['}']
 
     if n.right:
-        lines += ['%sif(x > %s) {' % ('else ' if n.left else '', n.value)]
-        lines += [' ' + x for x in triegen_C_int(n.right, fmt)]
-        lines += ['}']
+        if n.right.count() == 1:
+            lines += triegen_C_int(n.right, fmt)
+        else:
+            lines += ['%sif(x > %s) {' % ('else ' if n.left else '', n.value)]
+            lines += [' ' + x for x in triegen_C_int(n.right, fmt)]
+            lines += ['}']
 
     lines += [
         '%sif(%s == x) {' % ('else ' if n.left or n.right else '', n.value),
